bf127f43294b905bf8b2796e2266aa9ada7047bd
as280093 <as280093@gmail.com>
2025-01-12 00:53:30 +0530

    fix 404 error and deploy yml

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,22 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-  publicDir: "public",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    copyPublicDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
